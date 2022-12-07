ff2192ebacad8f1a38b617311aeac184f54d4477
Enzo <enzovivallo05@outlook.com>
2022-12-07 18:44:30 -0300

    refactor: media session helpers

--- src/lib/mediaSession.ts
+++ src/lib/mediaSession.ts
@@ -1,23 +1,23 @@
 import type Song from "./types/song";
 import type Picture from "./types/picture";
 
-export const updateMediaSessionSong = (song: Song, picture?: Picture) => {
-  if (!("mediaSession" in navigator)) return;
+export const supportsMediaSession = "mediaSession" in navigator;
 
-  const mediaMetadata: MediaMetadataInit = {
+export const updateMediaSessionSong = (song: Song, picture?: Picture) => {
+  if (!supportsMediaSession) return;
+
+  navigator.mediaSession.metadata = new MediaMetadata({
     title: song.title,
     artist: song.artist,
     album: song.album,
     artwork: picture
       ? [{ src: picture.url, sizes: "512x512", type: picture.mime_type }]
       : [],
-  };
-
-  navigator.mediaSession.metadata = new MediaMetadata(mediaMetadata);
+  });
 };
 
 export const resetMediaSession = () => {
-  if (!("mediaSession" in navigator)) return;
+  if (!supportsMediaSession) return;
 
   navigator.mediaSession.setPositionState(null);
   navigator.mediaSession.metadata = new MediaMetadata();
