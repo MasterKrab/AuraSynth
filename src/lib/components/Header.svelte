91cf42af3e6c3b914d964fd1c5f3d442bfce6294
Enzo <enzovivallo05@outlook.com>
2023-07-09 23:25:08 -0400

    feat: improve header styles

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,14 +3,41 @@
 </script>
 
 <header class="header">
-  <nav>
-    <a href="/" use:link>Songs</a>
-    <a href="/albums" use:link>Albums</a>
+  <nav class="navigation">
+    <ul class="list">
+      <li>
+        <a href="/" use:link>Songs</a>
+      </li>
+      <li>
+        <a href="/albums" use:link>Albums</a>
+      </li>
+    </ul>
   </nav>
 </header>
 
 <style>
   .header {
+    display: flex;
+    align-items: center;
+    background-color: var(--secondary-color);
+    padding-left: 0.5rem;
+    padding-right: 0.5rem;
     height: var(--header-height);
+    border-bottom: solid 0.125rem var(--tertiary-color-transparent);
+  }
+
+  .navigation {
+    width: 100%;
+  }
+
+  .list {
+    display: flex;
+    gap: 0.5rem;
+    padding-left: 0;
+    padding-bottom: 0.25rem;
+    font-size: 1.25rem;
+    color: var(--tertiary-color);
+
+    list-style: none;
   }
 </style>
