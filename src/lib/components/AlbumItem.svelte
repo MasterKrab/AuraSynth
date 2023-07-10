383f461026cd8328be211a93f95aa531477af169
Enzo <enzovivallo05@outlook.com>
2023-07-09 23:24:24 -0400

    feat: improve layout and song information

--- src/lib/components/AlbumItem.svelte
+++ src/lib/components/AlbumItem.svelte
@@ -5,13 +5,12 @@
 
   import music from '../stores/music'
   import Artwork from './Artwork.svelte'
+  import Svg from './Svg.svelte'
 
   export let album: Album
   export let loading: 'lazy' | 'eager' = 'eager'
 
-  const createHandleClick = (album: Album) => (event: MouseEvent) => {
-    if (event.detail !== 2) return
-
+  const handleClickPlay = () => {
     music.setAlbumToTracks(album)
   }
 </script>
@@ -20,15 +19,71 @@
   class="album"
   aria-label="Album"
   transition:fly={{ y: 50, duration: 500 }}
-  on:click={createHandleClick(album)}
 >
-  <Artwork url={album.artworkUrl} {loading} />
+  <div class="album__artwork-container">
+    <Artwork url={album.artworkUrl} {loading} />
+
+    <button class="album__button" on:click={handleClickPlay}>
+      <Svg
+        src="/assets/icons/play.svg"
+        width="1.75em"
+        fill="var(--tertiary-color)"
+      />
+    </button>
+  </div>
   <h2 class="album__title">{album.title || 'Unknown title'}</h2>
   <p class="album__artist">{album.artist || 'Unknown artist'}</p>
 </article>
 
 <style>
+  .album {
+    color: var(--tertiary-color);
+  }
+
+  .album__artwork-container {
+    position: relative;
+    transition: box-shadow 0.2s;
+  }
+
+  .album__artwork-container:hover,
+  .album__artwork-container:focus-visible {
+    box-shadow: 0 0 0.75em 0.25em var(--quaternary-color-transparent);
+  }
+
+  .album__artwork-container:hover .album__button {
+    opacity: 1;
+  }
+
+  .album__button {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 5em;
+    height: 5em;
+    background-color: var(--tertiary-color-transparent);
+    padding-left: 0.5rem;
+    border-radius: 50%;
+    transform: translate(-50%, -50%);
+    opacity: 0;
+    transition: opacity 0.5s, transform 0.5s;
+  }
+
+  .album__button:focus-visible {
+    opacity: 1;
+  }
+
+  .album__button:active {
+    transform: translate(-50%, -50%) scale(0.9);
+  }
+
   .album__title {
-    font-size: 1rem;
+    font-size: 0.9rem;
+    margin-bottom: 0.25rem;
+  }
+
+  .album__artist {
+    font-weight: 300;
+    font-size: 0.8rem;
+    margin-top: 0;
   }
 </style>
--- src/lib/components/Songs.svelte
+++ src/lib/components/Songs.svelte
@@ -2,26 +2,41 @@
   import type Song from '../types/song'
 
   import music from '../stores/music'
+  import SongArtwork from './SongArtwork.svelte'
+  import formatSeconds from '../formatSeconds'
 
   export let songs: Song[]
 
+  const createPlaySongDoubleClick = (song: Song) => (event: MouseEvent) => {
+    if (event.detail !== 2) return
+
+    music.setSongsToTracks(song)
+  }
+  createPlaySongDoubleClick
   const createPlaySong = (song: Song) => () => {
     music.setSongsToTracks(song)
   }
 </script>
 
-<article class="songs" aria-label="Songs">
+<article class="songs" aria-label="Songs" tabindex={0}>
   {#each songs as song}
-    <article tabindex={0} class="song">
+    <article class="song" on:click={createPlaySongDoubleClick(song)}>
       <button
+        class="button"
+        aria-label="Play song: {song.title}"
         on:click={createPlaySong(song)}
-        aria-label="Play song: {song.title}">Play</button
       >
-      <h2>{song.title}</h2>
-      <p>{song.artist}</p>
-      <p>{song.album}</p>
-      <p>{song.year}</p>
-      <p>{song.duration}</p>
+        <SongArtwork path={song.path} />
+      </button>
+      <h2 class="title">{song.title || 'Unknown title'}</h2>
+      <p class="text">{song.artist || 'Unknown artist'}</p>
+      <p class="text">{song.album || 'Unknown album'}</p>
+
+      {#if song.year}
+        <p class="text">{song.year}</p>
+      {/if}
+
+      <p class="text">{formatSeconds(song.duration)}</p>
     </article>
   {/each}
 </article>
@@ -30,16 +45,100 @@
   .songs {
     display: flex;
     flex-direction: column;
-    gap: 1rem;
+    font-size: 0.75em;
   }
 
   .song {
     display: flex;
-    gap: 1rem;
-    padding: 1rem;
+    align-items: center;
+    gap: 1.25rem;
+    padding: 0.25rem;
   }
 
   .song:focus {
     outline: 1px solid black;
+  }
+
+  .button {
+    position: relative;
+    min-width: 3.5rem;
+    width: 4rem;
+    max-width: 6rem;
+  }
+
+  .button::before,
+  .button::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 10;
+    opacity: 0;
+    transition: opacity 0.25s;
+  }
+
+  .button::before {
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+
+  .button::after {
+    background-image: url('/assets/icons/play.svg');
+    background-size: 40%;
+    background-repeat: no-repeat;
+    background-position: center;
+    filter: invert(1);
+    transition: opacity 0.2s transform 0.5s;
+  }
+
+  .button:active::after {
+    transform: scale(0.9);
+  }
+
+  .button:hover::before {
+    opacity: 1;
+  }
+
+  .button:hover::after {
+    opacity: 0.75;
+  }
+
+  .title,
+  .text {
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+
+  .title {
+    width: 40%;
+    font-size: 1em;
+  }
+
+  @media (min-width: 50rem) {
+    .title {
+      width: 100%;
+      font-size: 1.25em;
+    }
+  }
+
+  .text {
+    width: 25%;
+  }
+
+  @media (max-width: 20rem) {
+    .text:nth-child(3) {
+      position: absolute;
+      transform: scale(0);
+    }
+  }
+
+  @media (max-width: 50rem) {
+    .text:nth-child(4),
+    .text:nth-child(5),
+    .text:nth-child(6) {
+      position: absolute;
+      transform: scale(0);
+    }
   }
 </style>
