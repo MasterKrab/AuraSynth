3a4bdff29ecfb0d7fa8e8bf6948233012af06002
Enzo <enzovivallo05@outlook.com>
2023-07-22 21:36:10 -0400

    feat: improve header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,18 +1,53 @@
 <script lang="ts">
   import { link } from 'svelte-routing'
+
+  import Svg from './Svg.svelte'
+  import ReloadMusic from './ReloadMusic.svelte'
 </script>
 
 <header class="header">
   <nav class="navigation">
     <ul class="list">
       <li>
-        <a href="/" use:link>Songs</a>
+        <a class="link" href="/" use:link>
+          <Svg
+            src="/assets/icons/music-note-2.svg"
+            width="1.25rem"
+            height="1.25rem"
+            fill="var(--tertiary-color)"
+            aria-hidden="true"
+          />
+          <span class="link__text">Songs</span>
+        </a>
       </li>
       <li>
-        <a href="/albums" use:link>Albums</a>
+        <a class="link" href="/albums" use:link>
+          <Svg
+            src="/assets/icons/dvd-disk.svg"
+            width="1.25rem"
+            height="1.25rem"
+            fill="var(--tertiary-color)"
+            aria-hidden="true"
+          />
+          <span class="link__text">Albums</span>
+        </a>
+      </li>
+      <li>
+        <a class="link" href="/settings" use:link>
+          <Svg
+            src="/assets/icons/settings.svg"
+            width="1.25rem"
+            height="1.25rem"
+            fill="var(--tertiary-color)"
+            aria-hidden="true"
+          />
+          <span class="link__text">Settings</span>
+        </a>
       </li>
     </ul>
   </nav>
+
+  <ReloadMusic />
 </header>
 
 <style>
@@ -31,13 +66,21 @@
   }
 
   .list {
+    list-style: none;
     display: flex;
-    gap: 0.5rem;
+    gap: 1.5rem;
     padding-left: 0;
-    padding-bottom: 0.25rem;
     font-size: 1.25rem;
     color: var(--tertiary-color);
+  }
 
-    list-style: none;
+  .link {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+  .link__text {
+    margin-bottom: 0.1rem;
   }
 </style>
---
+++ src/lib/components/ReloadMusic.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import music from '../stores/music'
+
+  import Svg from './Svg.svelte'
+</script>
+
+{#if $music.loading}
+  <p
+    class="spinner"
+    role="alert"
+    aria-live="polite"
+    aria-label="Loading music..."
+  >
+    <Svg
+      src="/assets/icons/reload.svg"
+      width="1.25rem"
+      height="1.25rem"
+      fill="var(--tertiary-color)"
+      aria-hidden="true"
+    />
+  </p>
+{/if}
+
+<style>
+  @keyframes spin {
+    from {
+      transform: rotate(0);
+    }
+
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  .spinner {
+    width: 1.25rem;
+    height: 1.25rem;
+    animation: spin 1.25s linear infinite;
+  }
+</style>
